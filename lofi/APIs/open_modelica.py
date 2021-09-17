061d01e640c504d20160cc423fa5fff43871060b
LiborKudela <Libor.Kudela@vutbr.cz>
2021-09-17 13:58:39 +0200

    Refactoring OM xml_handler

--- lofi/APIs/open_modelica.py
+++ lofi/APIs/open_modelica.py
@@ -7,21 +7,21 @@ import os
 from subprocess import call, PIPE
 from DyMat import DyMatFile
 import pandas as pd
-from .py_api import Model_api
+from .model_api import Model_api
 
-class xml_init_file_handler():
+class init_file_handler():
     def __init__(self, init_file):
         self.init_file = init_file
-        self.tree = ET.parse(self.init_file)
         self.all = self.parse_xml()
 
     def parse_xml(self):
         all = []
         if os.path.exists(self.init_file):
-            self.root = self.tree.getroot()
+            tree = ET.parse(self.init_file)
+            root = tree.getroot()
             hashtag = rc(r"#(\w+)")
 
-            for sv in self.root.iter("ScalarVariable"):
+            for sv in root.iter("ScalarVariable"):
                 dict = {}
                 dict["name"] = sv.get("name")
                 dict["description"] = sv.get("description")
@@ -38,47 +38,37 @@ class xml_init_file_handler():
                 up = None
                 for att in ch:
                     start = att.get('start')
-                    lb = att.get("min")
-                    ub = att.get("max")
+                    _min = att.get("min")
+                    _max = att.get("max")
                 try:
                     dict["start"] = float(start)
                 except:
                     dict["start"] = None
-                dict["lower_bound"] = float(lb) if lb is not None else -1.0
-                dict["upper_bound"] = float(ub) if ub is not None else 1.0
+                dict["min"] = float(_min) if _min is not None else -1.0
+                dict["max"] = float(_max) if _max is not None else 1.0
                 all.append(dict)
             return all
 
-    def get_parameters(self, info_key):
+    def parameters(self, info_key, tags):
         parameters = []
         for sv in self.all:
             passes = sv["variability"] == "parameter"
             passes = passes and sv["start"] is not None
             passes = passes and sv["classType"] != "rSen"
-            passes = passes and any(tag in sv["hashtags"] for tag in ["optimize","optimise"])
+            passes = passes and any(tag in sv["hashtags"] for tag in tags)
             if passes:
                 parameters.append(sv[info_key])
         return parameters
 
-    def get_objectives(self, info_key):
+    def continuous(self, info_key, tags):
         objectives = []
         for sv in self.all:
             passes = sv["variability"] == "continuous"
             passes = passes and "der(" not in sv["name"]
-            passes = passes and any(tag in sv["hashtags"] for tag in ["objective"])
+            passes = passes and any(tag in sv["hashtags"] for tag in tags)
             if passes:
                 objectives.append(sv[info_key])
         return objectives
-
-    def get_plot_variables(self, info_key):
-        variables = []
-        for sv in self.all:
-            passes = sv["variability"] == "continuous"
-            passes = passes and "der(" not in sv["name"]
-            passes = passes and any(tag in sv["hashtags"] for tag in ["plot"])
-            if passes:
-                variables.append(sv[info_key])
-        return variables
 
 class open_modelica(Model_api):
     def __init__(self, files, model,
@@ -86,7 +76,8 @@ class open_modelica(Model_api):
                  abort_slow=0,
                  solver='dassl',
                  tmp_storage=None,
-                 init_statei=None):
+                 init_state=None,
+                 stop_time=None):
 
         # resolve file paths
         files = [files] if type(files) is not list else files
@@ -114,21 +105,23 @@ class open_modelica(Model_api):
 
         self.abort_slow = abort_slow
         self.solver = solver
+        self.stop_time = stop_time
         cluster.comm.Barrier()  # synchronize here
 
-        # read init_file for information about the compiled model
-        self.xml_info = xml_init_file_handler(self.init_file)
+        # read init_file.xml for variable info of the compiled model
+        self.init_file = init_file_handler(self.init_file)
 
         # get parameter info from within parsed init_file
         # this needs to be here before call to super().__init__()
-        self.y_names = self.xml_info.get_objectives("name")
-        self.p_names = self.xml_info.get_parameters("name")
-        self.p_lb = np.array(self.xml_info.get_parameters("lower_bound"))
-        self.p_ub = np.array(self.xml_info.get_parameters("upper_bound"))
-        self.p_start = self.xml_info.get_parameters("start")
+        self.y_names = self.init_file.continuous("name", ["objective"])
+        self.p_names = self.init_file.parameters("name", ["optimize"])
+        self.p_lb = np.array(self.init_file.parameters("min", ["optimize"]))
+        self.p_ub = np.array(self.init_file.parameters("max", ["optimize"]))
+        self.p_start = self.init_file.parameters("start", ["optimize"])
 
-        # get variables to be plotted from withing init_file
-        self.plot_vars = self.xml_info.get_plot_variables("name")
+        # get input/output names
+        self.input_names = self.init_file.continuous("name", ["input"])
+        self.output_names = self.init_file.continuous("name", ["output"])
 
         super().__init__()
 
@@ -198,34 +191,54 @@ class open_modelica(Model_api):
             flags += f" -alarm={self.abort_slow}"
         return self.compiled_file + flags
 
-    def get_result_path(self, id, key="tag"):
+    def get_result_path(self, tag):
         """Construct result path based on situation"""
-        return self.result_dir + f"/{key}_{id}.mat"
+        return self.result_dir + f"/tag_{tag}.mat"
 
-    def result_file_flag(self, id, key="tag"):
+    def result_file_flag(self, tag):
         """Constructs result path flag for the model executable"""
-        return " -r=" + self.get_result_path(id)
+        return " -r=" + self.get_result_path(tag)
 
     def override_parameters(self, p):
         """Construcst flag that overrides parameters in the model executable"""
         flag = " -override "
+        flag += f"stopTime={self.stop_time}"
         for i in range(self.m):
             flag += f"{self.p_names[i]}={p[i]},"
         return flag
 
-    def evaluate(self, prms, result_tag):
-        """Build shell command with all flags and runs the executable"""
+    def override_flag(self, inputs, parameters):
+        flag = " -override "
+        for key in inputs.keys():
+            flag += f"{key}={value},"
+        for i in range(self.m):
+            flag += f"{self.p_names[i]}={p[i]}," 
+        return flag
+
+    def evaluate(self, prms=None, x=None):
+        """Runs OM model with inputs x and parameters prms."""
+        self.result_tag = cluster.status.Get_tag()
         command = self.get_simulation_command_root()
-        command += self.result_file_flag(result_tag)
+        command += self.result_file_flag(self.result_tag)
         command += self.override_parameters(prms)
         return call(command, shell=True, stdout=PIPE, stderr=PIPE)
 
-    def get_tagged_result(self, id, key="tag"):
+    def evaluate(self, prms):
+        """Build shell command with all flags and runs the executable"""
+        self.result_tag = cluster.status.Get_tag()
+        command = self.get_simulation_command_root()
+        command += self.result_file_flag(self.result_tag)
+        command += self.override_parameters(prms)
+        return call(command, shell=True, stdout=PIPE, stderr=PIPE)
+
+    def acquire_result(self, result_id=None):
         """Loads the result file dropped by the executable"""
-        return DyMatFile(self.get_result_path(id))
+        if result_id is None:
+            result_id = self.result_tag
+        return DyMatFile(self.get_result_path(result_id))
 
     def extract_raw_loss(self, result):
-        """Extracts arrays coresponding to all vars. marked as #objective"""
+        """Extracts arrays coresponding to all init_file. marked as #objective"""
         return result.getVarArray(self.y_names, withAbscissa=False)[0:,:]
 
     @cluster.on_master
